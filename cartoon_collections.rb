71b419990da4c980abfb60ebdb8942c6fe24c897
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-02 20:27:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  i = 0
+  while i < dwarves.length
+    dwarves.each_with_index do |dwarf|
+      puts dwarf
+    end
+  end
 end
 
 def summon_captain_planet# code an argument here
